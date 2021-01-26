f707abda978ca28ea7cabdc2bdd773bd4f818796
DjBee0312 <pashapasha0312@gmail.com>
2021-01-26 13:49:56 +0200

    Basic implementation of the game

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DragAndDrop : MonoBehaviour
+{
+    private bool moveAllowed;
+
+    private Collider2D collider;
+    private StartGame sg;
+
+    public GameObject DeathEffect;
+    public GameObject SlectionRandomEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sg = GameObject.FindGameObjectWithTag("SG").GetComponent<StartGame>();
+        collider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Collider2D touchedCollider = Physics2D.OverlapPoint(touchPosition);
+                if (collider == touchedCollider)
+                {
+                    Instantiate(SlectionRandomEffect, transform.position, Quaternion.identity);
+                    moveAllowed = true;
+                }
+            }
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                if (moveAllowed)
+                {
+                    transform.position = new Vector2(touchPosition.x, touchPosition.y);
+                }
+            }
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                moveAllowed = false;
+            }
+
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Planet")
+        {
+            Instantiate(DeathEffect, transform.position, Quaternion.identity);
+            sg.GameOver();
+            Destroy(other.gameObject);
+        }
+    }
+}
